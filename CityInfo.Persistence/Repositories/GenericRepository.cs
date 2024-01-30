a49a9241d81f13f2d04e19ae1b06806b90dce44e
NetNinjaEngineer <me5260287@outlook.com>
2024-01-30 18:52:37 +0200

    Implementing generic repository

--- CityInfo.Persistence/Repositories/GenericRepository.cs
+++ CityInfo.Persistence/Repositories/GenericRepository.cs
@@ -1,34 +1,56 @@
 ﻿using CityInfo.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace CityInfo.Persistence.Repositories;
-public class GenericRepository<T>(CityInfoDbContext context)
-    : IGenericRepository<T> where T : class
+public class GenericRepository<T> : IGenericRepository<T> where T : class
 {
-    private readonly CityInfoDbContext _context = context;
+    private readonly CityInfoDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepository(CityInfoDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+
 
     public T Create(T entity)
     {
-        throw new NotImplementedException();
+        _dbSet.Add(entity);
+        return entity;
     }
 
     public T Delete(T entity)
     {
-        throw new NotImplementedException();
+        _dbSet.Remove(entity);
+        return entity;
     }
 
     public IEnumerable<T> GetAll(bool trackChanges, params Expression<Func<T, object>>[] includes)
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = _dbSet;
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        return !trackChanges ? query.AsNoTracking() : query;
     }
 
     public IEnumerable<T> GetAllByCondition(bool trackChanges, Expression<Func<T, bool>> expression, params Expression<Func<T, object>>[] includes)
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = _dbSet;
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        return !trackChanges ?
+        query.Where(expression)
+        .AsNoTracking() : query.Where(expression);
     }
 
     public T Update(T entity)
     {
-        throw new NotImplementedException();
+        var updatedEntity = _dbSet.Update(entity);
+        return updatedEntity.Entity;
     }
 }
